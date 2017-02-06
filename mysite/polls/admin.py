0c873b25733b65ab597cc87732b94c4d951a2289
Mark <twcloudwebapp@outlook.com>
2017-02-06 11:49:39 +0800

    dummy index and result

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,12 +3,16 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+# class ChoiceInline(admin.StackedInline):
+# https://docs.djangoproject.com/en/1.10/intro/tutorial07/
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
